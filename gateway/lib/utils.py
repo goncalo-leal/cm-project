fa2deee0780cf92f1b41408132ead5a4f130acbf
BabaMonteiro <barbarammoreira@ua.pt>
2023-12-09 01:31:26 +0000

    setup function

--- gateway/lib/utils.py
+++ gateway/lib/utils.py
@@ -19,8 +19,30 @@ PROTOCOLS = {
 }
 
 # ----------------------------------
+def setUp():
 
+    lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
 
+    s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+    s.setblocking(False)
+
+    lora_dict = {
+        'mode': LoRa.LORA,
+        'region': LoRa.EU868,
+        'mac': lora.mac()
+    }
+    # print("in utils.py, mac: " + str(lora.mac()))
+
+    socket_dict = {
+        'object': s,
+        'type': 'AF_LORA',
+        'mode': 'SOCK_RAW',
+        'blocking': False
+    }
+
+    return lora_dict, socket_dict
+
+ 
 def parse_packet(packet):
     id = struct.unpack('!B', packet[:1])[0]
 
@@ -36,6 +58,26 @@ def compose_packet(data):
 
     return struct.pack(PROTOCOLS[data[0]], *data)
 
+
+# def icmp(id, src, dest):
+#     if len(src) != 8 or len(dest) != 8:
+#         raise Exception('Invalid MAC address')
+    
+#     request = compose_packet([0x0, 16, src, dest])
+#     reply = compose_packet([0x1, 16, src, dest])
+#     timeout = time.time() + 5
+
+#     if id == 0:
+#         s.send(request)
+#         while time.time() < timeout:
+#             packet = s.recv(64)
+#             if packet:
+#                 data = parse_packet(packet)
+#                 if data[0] == 0x1 and data[3] == src:
+#                     return data[2]
+#     if id == 1:
+#         s.send(reply)
+#     return None
 
 def icmp_request(src, dest):
     if len(src) != 8 or len(dest) != 8:
@@ -62,3 +104,5 @@ def icmp_reply(src, dest):
 
     reply = compose_packet([0x1, 16, src, dest])
     s.send(reply)
+
+
--- gateway/main.py
+++ gateway/main.py
@@ -1,28 +1,35 @@
 # main.py -- put your code here!
-from network import LoRa
+# from network import LoRa
+import network
 import socket
 import time
 import pycom
 #from lib.utils import parse_packet,compose_packet,icmp_request
-from utils import parse_packet,compose_packet,icmp_request
+from utils import parse_packet,compose_packet,icmp_request, setUp
 
-# COM4
-
+# LoRa Configuration:
 lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
-mac = lora.mac()
 
+# Socket Configuration:
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.setblocking(False)
 
-
+# COM4
 
 known_nodes = [b'p\xb3\xd5I\x99x1_']
 active_nodes = []
+
+
+# info, socket_info = setUp()
 
 while True:
     print('Sending request')
 
     for node in known_nodes:
+        # get the mac from the info dictionary
+        # mac = info['mac']
+        # print("In main.py, mac:" + str(mac))
+
         request = icmp_request(lora.mac(),node)
 
         if request:
